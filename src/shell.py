4d9992d5cdfbcad4cf8fe5f0ea3b257504605fc8
0x0is1 <whitehats@localhost.localdomain>
2020-04-16 16:57:08 +0530

    lil stuff

--- src/shell.py
+++ src/shell.py
@@ -4,47 +4,24 @@ from data_collect import data_collector
 from formal import formal
 from visualize import visualize
 import os
-from default import default_data
+import random
+from default_data import default_data
+user = 'user@'+str(random.randint(4000, 9999))+'> '
+cases = {0:"exit",1:"cls",2:"get",3:"help",4:"set",5:"visualize",6: "ftp",7: "show options"}
+containers = {1:"id=", 2:"type=", 3:"report=", 4:"doc="}
 def main_function():
-
     formal()
     while True:
-        raw_command = input("> ")
+        command = list(map(str, input(user).split()))
+        if command[0] == cases[4]:
+            for i in range(4):
+                for j in range(len(command)):
+                    try:
+                        if containers[i] in command[j]:
+                            arg1 = command[j].split('=')
+                            print(arg1[1])
+                    except:
+                            pass
+                
 
-        try:
-            command, details = raw_command.split(" ")
-            print(command)
-            print(details)
-
-        except:
-            command = raw_command
- 
-        cases = {0:"exit",1:"cls",2:"get",3:"help",4:"set",5:"visualize",6: "ftp",7: "show options"}
-
-           ###############
-        try:
-            genefeature, slfeature, pfeature, cfeature, sofeature, comment = data_collector(str(default_data.sample_id), str(default_data.sample_type), str(default_data.report_type), str(default_data.doc_type))
-
-        except:
-            data, url = data_collector(str(default_data.sample_id), str(default_data.sample_type), str(default_data.report_type), str(default_data.doc_type))
-
-        if command == None:
-            print('No command supplied. type help to see all commands')
-
-        if command == cases[0]:
-            exit(0)
-
-        if command == cases[1]:
-            os.system('clear')
-            formal()
-
-        if command == cases[2]:
-            print(data)
-
-        if command == cases[5]:
-            print (url)   
-            visualize(url)                     
-
-        else:
-            os.system(command)
-        
+main_function()
