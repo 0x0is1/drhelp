8029244ba40c8153ae257ac7673c4bc473fb95ac
0x0is1 <whitehats@localhost.localdomain>
2020-05-01 21:30:56 +0530

    added options for new features

--- src/options.py
+++ src/options.py
@@ -2,4 +2,5 @@ from default_data import default_data
 values={1:str(default_data.sample_id),2:default_data.sample_type,3:default_data.report_type,4:default_data.doc_type }
 set_options = {1:"id", 2:"type", 3:"report", 4:"doc"}
 get_options = {1: "name", 2: "intro", 3: "gene", 4: "stem-loop", 5: "peptide", 6: "cds", 7: "source", 8: "comment", 9: "all"}
-cases = {0:"exit",1:"cls",2:"get",3:"help",4:"set",5:"visualize",6: "ftp",7: "options", 8: "fetch"}
+cases = {0:"exit",1:"cls",2:"get",3:"help",4:"set",5:"visualize",6: "ftp",7: "options", 8: "fetch", 9: "searchd", 10: "searchl"}
+error_key = {0: "Your browsing activity is empty.", 1: "Error404"}
